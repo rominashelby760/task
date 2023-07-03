b36657e75e0ed1fa4ba99bafc3b254e5042b0f1b
rominashelby760 <138257422+rominashelby760@users.noreply.github.com>
2023-07-03 22:27:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+def longest_common_subsequence(str1, str2):
+    m = len(str1)
+    n = len(str2)
+
+    # Создаем матрицу размером (m+1)x(n+1) для хранения длин подпоследовательностей
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Восстановление наибольшей общей подпоследовательности
+    lcs = ""
+    i, j = m, n
+    while i > 0 and j > 0:
+        if str1[i - 1] == str2[j - 1]:
+            lcs = str1[i - 1] + lcs
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return lcs
+
+str1 = "ABCDGH"
+str2 = "AEDFHR"
+lcs = longest_common_subsequence(str1, str2)
+print("Наибольшая общая подпоследовательность:", lcs)
